d01db0d30a61698f4f0e42faff3bf62bd56b649a
Marius van der Wijden <m.vanderwijden@live.de>
2024-06-18 16:28:33 +0200

    dex: final example

--- dex.sol
+++ dex.sol
@@ -18,43 +18,36 @@ contract DEX {
     }
 
     function EthBalance() internal view returns (uint256) {
-        return address(this).balance;
+        return address(this).balance - msg.value;
     }
 
     function getUnits(uint256 inputTokens, uint256 inputReserves, uint256 outputReserves) internal pure returns (uint256) {
         require(inputReserves > 0 && outputReserves > 0, "not enough funds");
 
-        uint256 inputWithFee = inputTokens * 999
+        uint256 inputWithFee = inputTokens * 999;
         uint256 numerator = inputWithFee * outputReserves;
-        uint256 denominator = (inputWithFee * 1000) + inputTokens;
+        uint256 denominator = (inputWithFee * 1000) + inputReserves;
 
         return numerator / denominator; // (x*y)/(z+x)
     }
 
-    function swapTokenToETH(uint256 tokens) public {
-        uint256 price = getUnits(tokens, TokenBalance(), EthBalance());
-        token.transferFrom(msg.sender, address(this), tokens);
-        payable(msg.sender).transfer(price);
-    }
-
     function swapETHToToken() public payable {
-        uint256 price = getUnits(msg.value, EthBalance()-msg.value, TokenBalance());
-        token.transfer(msg.sender, price);
+        uint256 units = getUnits(msg.value, EthBalance(), TokenBalance());
+        token.transfer(msg.sender, units);
     }
-
+    
+    function swapTokenToETH(uint256 tokens) public {
+        uint256 units = getUnits(tokens, TokenBalance(), EthBalance());
+        payable(msg.sender).transfer(units);
+    }
 
     function addLiquidity(uint256 tokens) public payable {
-        uint256 ethBalance = EthBalance() - msg.value;
-        uint256 tokenBalance = TokenBalance();
-
-        if (tokenBalance == 0) {
+        if (TokenBalance() == 0) {
             token.transferFrom(msg.sender, address(this), tokens);
             return;
         }
-
-        uint256 price = getUnits(msg.value, ethBalance, tokenBalance);
-        require(tokens >= price, "not enough tokens send");
-
-        token.transferFrom(msg.sender, address(this), price);
+        uint256 units = getUnits(msg.value, EthBalance(), TokenBalance());
+        require(tokens >= units, "not enough funds");
+        token.transferFrom(msg.sender, address(this), units);
     }
 }
